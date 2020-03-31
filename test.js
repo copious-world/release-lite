7be2f1116a09f8a0770547d12fff77d0744e5c4d
Richard Albert Leddy <richardaleddy@gmail.com>
2020-03-31 14:51:25 -0700

    A few updates to make sure that no fixed files names are in use.

--- test.js
+++ test.js
@@ -8,6 +8,34 @@ if ( g_ssh_pass === undefined ) {
     console.error('No ssh password provided')
     process.exit(1)
 }
+let ecosystem = `
+    module.exports = {
+        apps : [
+            {
+                name        : "my-app-name",
+                script      : "/my-app-path/myapp.js",
+                watch       : true,
+                env: {
+                    "NODE_ENV": "development",
+                },
+                env_production : {
+                    "NODE_ENV": "production"
+                }
+            },
+            {
+                name        : "etc",
+                script      : "/etc.js",
+                watch       : true,
+                env: {
+                    "NODE_ENV": "development",
+                },
+                env_production : {
+                    "NODE_ENV": "production"
+                }
+            }
+        ]
+      }
+      `
 
-var releaser = new Releaser(g_ssh_pass,g_release_json_file)
+var releaser = new Releaser(g_ssh_pass,g_release_json_file,ecosystem)
 releaser.run()
