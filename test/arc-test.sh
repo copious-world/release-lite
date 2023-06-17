d70562586f994e88cd3ac8cbf72f9efdf7460da0
Richard Albert Leddy <richardaleddy@gmail.com>
2023-06-16 17:18:03 -0700

    Update arc-test.sh

--- test/arc-test.sh
+++ test/arc-test.sh
@@ -13,7 +13,7 @@ pushd $DIR
 GRAPH_FLDS='name,depth,sibs,user,pass,addr,backrefs,upload_scripts,download_scripts,ops,op_dir,y_fingerprint,post_ops'
 # for startup
 GRAPH_STR=$(cat <<EOL
-here,1,home otw copious popsong ,richardalbertleddy,dGVzdDd0ZXN0Cg==,localhost,./arc-ops,1,none
+here,1,home otw copious popsong,richardalbertleddy,dGVzdDd0ZXN0Cg==,localhost,./arc-ops,1,none
 home,2,g_sessions contacts endpoint-users otw-create,richard,dGVzdDR0ZXN0Cg==,76.229.181.242,./arc-ops,1,here
 g_sessions,3,none,root,ZGlldHBpCg==,192.168.1.71,/home/arc-ops,0,home here
 contacts,3,none,root,ZGlldHBpCg==,192.168.1.75,/home/arc-ops,0,home here
@@ -34,7 +34,7 @@ EOL
 
 
 GRAPH_STR=$(echo "$GRAPH_STR" | base64)
-OPS_STR=$(echo 'pwd\nls' | base64)
+OPS_STR=$(echo 'pwd\nls' | base64)                      # pwd ls for setup, just one 
 UPLOADER="setup_uploader.sh"
 
 PASS='TDJ2PTk1LHFbKTR3Ri0K'
